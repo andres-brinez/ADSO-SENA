e1d54f0064c8acd2ed867b7fd9f3ed187fd71973
Andres Briñez <brinezlopez08@gmail.com>
2024-03-26 13:38:55 -0500

    Se crea el módelo que representa un producto

---
+++ Proyect/src/modelo/Producto.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author brine
+ */
+public class Producto {
+    
+    
+    public  int codigo;
+    public String nombre;
+    public int precio;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+}
