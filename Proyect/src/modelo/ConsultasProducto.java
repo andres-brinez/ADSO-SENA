2c9c3fe2839fe05793e3a337def063f5eae6a6a6
Andres Briñez <brinezlopez08@gmail.com>
2024-03-26 17:13:45 -0500

    Se crean las consultas de los  productos que se hacen a la base de datos

--- Proyect/src/modelo/ConsultasProducto.java
+++ Proyect/src/modelo/ConsultasProducto.java
@@ -5,27 +5,44 @@
 package modelo;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 
 /**
  *
  * @author brine
  */
-public class ConsultasProducto extends ConexionMysql  {
-    
-    modelo.ConexionMysql con = new ConexionMysql();
-    Connection  cn = con.conectar();
-    
-    
-    // Metodo para agregar un producto
-    
-    public void agregarProducto (Producto p){
-        
-        String consulta = "INSERT INTO `app`.`producto`(`codigo`,`nombre`,`precio`)VALUES('"+p.getCodigo()+"', '"+p.getNombre()+"', '"+p.getPrecio()+"')";
+public class ConsultasProducto extends ConexionMysql {
 
-        con.ejecutarConsultaSinRetorno(consulta,"Producto agregado");
-    
-   
+    modelo.ConexionMysql con = new ConexionMysql();
+    Connection cn = con.conectar();
+
+    // Método para agregar un producto
+    public void agregarProducto(Producto p) {
+        String consulta = "INSERT INTO app.producto (codigo, nombre, precio) VALUES ('" + p.getCodigo() + "', '" + p.getNombre() + "', '" + p.getPrecio() + "')";
+
+        con.ejecutarConsultaSinRetorno(consulta, "Producto agregado");
     }
-    
-    
+
+    // Método para actualizar un producto
+    public void actualizarProducto(Producto p) {
+        String consulta = "UPDATE app.producto SET nombre = '" + p.getNombre() + "', precio = '" + p.getPrecio() + "' WHERE codigo = '" + p.getCodigo() + "'";
+
+        con.ejecutarConsultaSinRetorno(consulta, "Producto actualizado");
+    }
+
+    // Método para eliminar un producto
+    public void eliminarProducto(int idProducto) {
+        String consulta = "DELETE FROM app.producto WHERE codigo = '" + idProducto + "'";
+
+        con.ejecutarConsultaSinRetorno(consulta, "Producto eliminado");
+    }
+
+    // Método para buscar un producto
+    public ResultSet buscarProducto(int idProducto) {
+        String consulta = "SELECT * FROM app.producto WHERE codigo = '" + idProducto + "'";
+
+        ResultSet resultado = con.ejecutarConsultaConRetorno(consulta, "Producto encontrado");
+
+        return resultado;
+    }
 }
